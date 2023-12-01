090a91f25542f56f429bc627da6f12c5c1f6e85d
Paul <kokot.p@e80group.com>
2023-12-01 16:18:17 +0100

    divide day 1 in two parts

---
+++ 01a/Program.fs
@@ -0,0 +1,14 @@
+﻿open System.IO
+open System
+
+let rec firstNumberInLine (l: char list) : int = 
+    match l with
+    | [] -> 0
+    | x::xs -> if Char.IsNumber(x) then (int x - int '0') else firstNumberInLine xs
+let twoDigitNumberInLine l = (firstNumberInLine l) * 10 + firstNumberInLine (l |> List.rev)
+let sumAllLinesInFile x =
+    (File.ReadAllLines(x) |> Array.toList) 
+    |> List.map (fun x -> twoDigitNumberInLine (Seq.toList(x))) 
+    |> List.sum
+
+printf "%i" (sumAllLinesInFile "Input.txt")
