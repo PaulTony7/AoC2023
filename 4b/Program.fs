15e2d9c0ada20f9fda6c831b0603fb0547effae8
Paul <kokot.p@e80group.com>
2023-12-04 09:45:07 +0100

    day 4 part 2 complete

---
+++ 4b/Program.fs
@@ -0,0 +1,35 @@
+﻿open System.IO
+open System
+open System.Collections.Generic
+
+let isNumberWinning (x:int) (winning:int list) =
+    if winning |> List.contains x then 1 else 0
+
+let filePath = "Input.txt"
+let splitInput =
+     File.ReadAllLines(filePath) 
+    |> Array.toList 
+    |> List.map (fun x -> x.Split([|':';'|'|]))
+let accumulatedCards = new List<int>(splitInput.Length)
+for i in 1..splitInput.Length do
+    accumulatedCards.Add(1)
+printf "length %i\n" accumulatedCards[1]
+
+let collectNumbers (i:int) = 
+    splitInput
+    |> List.map (fun x -> x[i])
+    |> List.map (
+        fun x -> 
+            (x.Split([|' '|], StringSplitOptions.RemoveEmptyEntries)) 
+            |> Array.toList 
+            |> List.map (fun y -> y |> int))
+let runningNumbers = collectNumbers 1
+let winningNumbers = collectNumbers 2
+
+(runningNumbers, winningNumbers) ||> List.mapi2 (fun i x z ->
+(0, x) ||> List.fold (fun acc y -> acc + isNumberWinning y z))
+|> List.iteri (fun i e -> 
+    for j in 1..e do
+        accumulatedCards[i+j] <- accumulatedCards[i] + accumulatedCards[i+j] 
+)
+accumulatedCards.ToArray() |> Array.toList |> List.sum |> printf "sum cards %i\n"
