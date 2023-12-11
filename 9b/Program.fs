9ec09cb0782b34db91b17da5833a865c9481ae49
Paul <kokot.p@e80group.com>
2023-12-11 09:53:31 +0100

    day 9 part 2 complete

---
+++ 9b/Program.fs
@@ -0,0 +1,22 @@
+﻿open System.IO
+open System
+open System.Linq
+
+let lines = 
+    File.ReadAllLines("Input.txt") 
+    |> Array.toList 
+    |> List.map (fun x -> x.Split([|' '|]) |>  Array.toList |> List.map (fun x -> x |> int64 |> bigint ))
+
+let isAllZero (l:bigint list) = 
+    l |> List.forall (fun x -> x=0I)
+let rec calculateDifferenceList (l:bigint list) = 
+    match l with
+    | x::y::_ -> y - x::calculateDifferenceList l.Tail
+    | _ -> []
+
+let rec getNextInSequence (l:bigint list) =
+    if isAllZero l then 0I
+    else 
+        l.Head - ((calculateDifferenceList l) |> getNextInSequence)
+
+lines |> List.map (fun x -> getNextInSequence x) |> List.sum |> printfn "sum %A" 
