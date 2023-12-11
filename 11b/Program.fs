11870943c769320ae68a11c28d733d4bc17ffda0
Paul <PaulKokot98@gmail.com>
2023-12-11 20:35:48 +0100

    Day 11 part 2 complete

--- 11b/Program.fs
+++ 11b/Program.fs
@@ -2,58 +2,57 @@
 open System.Linq
 
 type Vector2 =
-    val x:int;
-    val y:int;
+    val x:bigint;
+    val y:bigint;
     new (x, y) =
         {
             x=x;
             y=y;
         }
-let distanceMul = 2
+let distanceMul = 1000000I
 let lines = 
-    File.ReadAllLines("C:\Users\kokot.p\Documents\GitHub\AoC2023\11b\Input.txt") 
+    File.ReadAllLines("Input.txt") 
     |> Array.toList 
     |> List.map (fun x -> x.ToCharArray() |> Array.toList)
-// List.iter (fun x -> printfn "%s" x) lines
+    
 let lineEmpty l =
     List.forall (fun x -> x = '.') l
+
 let rec findHorizontalLines i (l)  =
     match l with
     | x::xs -> 
-        if (lineEmpty x) then i::findHorizontalLines (i+1) xs 
-        else findHorizontalLines (i+1) xs 
+        if (lineEmpty x) then i::findHorizontalLines (i+1I) xs 
+        else findHorizontalLines (i+1I) xs 
     | _ -> []
+
 let rec verticalSlice (l: char list list) (i:int) =
     match l with
     | x::xs -> x[i]::verticalSlice xs i
     | _ -> []
+
 let rec rotate90 (l: char list list) =
     List.mapi (fun i x -> verticalSlice l i) l[0]
-let implode (xs:char list) =
-        let sb = System.Text.StringBuilder(xs.Length)
-        xs |> List.iter (sb.Append >> ignore)
-        sb.ToString()
 
 let horizontalLines = 
-    findHorizontalLines 0 lines
+    findHorizontalLines 0I lines
 
 let verticalLines =
     rotate90 lines 
-    |> findHorizontalLines 0 
-    // |> rotate90 
-let rec findGalaxies l i j =
+    |> findHorizontalLines 0I 
+    
+let rec findGalaxies l i i2 j =
     let j2 = horizontalLines |> List.filter (fun x-> x < j)
-    let j2 = j2.Length*(distanceMul-1)+j
+    let j2 = bigint j2.Length*(distanceMul-1I)+j
     match l with
     | x::xs -> 
-        if x = '#' then new Vector2(i,j2)::findGalaxies xs (i+1) j2
+        if x = '#' then new Vector2(i2,j2)::findGalaxies xs (i+1I) (i2+1I) j
         else 
-            if verticalLines.Any((fun x-> x = i)) then findGalaxies xs (i+distanceMul) j2
-            else findGalaxies xs (i+1) j2
+            if verticalLines.Any((fun x-> x = i)) then findGalaxies xs (i+1I) (i2+distanceMul) j
+            else findGalaxies xs (i+1I) (i2+1I) j
     | _ -> []
 let galaxies = 
     lines 
-    |> List.mapi (fun j x -> (findGalaxies x 0 j)) 
+    |> List.mapi (fun j x -> (findGalaxies x 0I 0I (bigint j))) 
     |> List.collect id
 
 let distance (x:Vector2) (y:Vector2) =
@@ -61,7 +60,7 @@ let distance (x:Vector2) (y:Vector2) =
 
 let rec calculateDistance (l:Vector2 list) =
     match l with 
-    | x::xs -> (l |> List.fold (fun acc y -> acc + (distance x y)) 0 )::(calculateDistance xs)
+    | x::xs -> (l |> List.fold (fun acc y -> acc + (distance x y)) 0I )::(calculateDistance xs)
     | _ -> []
 let calcD = calculateDistance galaxies |> List.sum
-printfn "Total distance sum %i" calcD
+printfn "Total distance sum %A" calcD
